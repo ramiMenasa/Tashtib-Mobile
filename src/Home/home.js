a5af78b2156e3dce779c0a45b503cae9349d0d14
ramiMenasa <ramimenasa@gmail.com>
2023-03-10 18:04:42 +0200

    update drawer

--- src/Home/home.js
+++ src/Home/home.js
@@ -239,23 +239,7 @@ function Home({ navigation }) {
 
                         <View style={{ alignItems: 'center' }}>
                             <View style={{ flexDirection: 'row' }} >
-                                {/* <SelectDropdown
-                                    style={styles.input}
-                                    data={countries}
-                                    onSelect={(selectedItem, index) => {
-                                        console.log(selectedItem, index)
-                                    }}
-                                    buttonTextAfterSelection={(selectedItem, index) => {
-                                        // text represented after item is selected
-                                        // if data array is an array of objects then return selectedItem.property to render after item is selected
-                                        return selectedItem
-                                    }}
-                                    rowTextForSelection={(item, index) => {
-                                        // text represented for each item in dropdown
-                                        // if data array is an array of objects then return item.property to represent item in dropdown
-                                        return item
-                                    }}
-                                /> */}
+                                
                                 
                             <Picker
                                 selectedValue={sortValue}
